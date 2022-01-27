0c0b5c2358a127f7192dda864750a3f5e5f79a64
Juxyo <94303202+Juxyo@users.noreply.github.com>
2022-01-27 16:53:39 +0100

    mise a jour de la requete sql

--- admin.php
+++ admin.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="fr">
     <head>
@@ -7,52 +6,30 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Page admin</title>
     </head>
-    <?php
-        if(session_status() != PHP_SESSION_ACTIVE){
-            session_start();
-        }
-        require "data/db_login.php";
-    
-        if(isset($_SESSION['connected'])){
-    
-            $connexion=mysqli_connect($host,$login,$mdp,$bdd) or die("connexion impossible");
-    
-            $req="select users.username,edits.name from users,edits where edits.disp = 0 and edits.userid = users.userid";
-    
-            $stmt = mysqli_prepare($connexion, $req);
-            mysqli_stmt_bind_param($stmt, "ss", $_SESSION['connected']);
-            mysqli_stmt_execute($stmt);
-            $result = mysqli_stmt_get_result($stmt);
-            mysqli_stmt_close($stmt);
-
-            if(mysqli_num_rows($result)>=1){
-                foreach($result as $ligne) $return = $ligne;
-            }else{
-                $return = NULL;
-            }
-            
-            
-            for ($i, i<len($return){
-
-            }
-
-        }
-        ?>
     <body>
         <h1>Page administration des outils professeur</h1>
+        <table id="tabEmpreint">
         <?php
-        echo "<table id="tabEmpreint">";
-        for ($i; i<len($return; i++){
-            echo"<tr>
-            <td>
-            $return[i][0]
-            </td>
-            <td>
-            $return[i][1]
-            </td>
-            </tr>";
-        }
-        echo"</table>"
-        ?>
+            if(session_status() != PHP_SESSION_ACTIVE){
+                session_start();
+            }
+            require "data/dblogin.php";
+        
+            if(isset($_SESSION['connected'])){
+        
+                $connexion=mysqli_connect($host,$login,$mdp,$bdd) or die("connexion impossible");
+        
+                $req="select users.username,edits.name from users,edits where edits.disp = 0 and edits.empreint = users.userid";
+        
+                $stmt = mysqli_query($connexion, $req) or die('erreur');
+
+                while($ligne = mysqli_fetch_array($stmt, MYSQLI_ASSOC)){    
+                    echo "<tr>";
+                    echo "<td>".$ligne['name']."</td>";
+                    echo "<td>".$ligne['username']."</td>";
+                    echo "</tr>";
+                }
+            ?>
+            </table>
     </body>
 </html>
