1efe79471e252f692c4d5da5ced1f6fa4f4bd5db
Luckar <73293157+LuckarSorin@users.noreply.github.com>
2022-01-27 15:56:03 +0100

    ok test but projetWeb

--- data/makeDatabase.sql
+++ data/makeDatabase.sql
@@ -15,20 +15,27 @@ create table IF NOT EXISTS projetWeb.edits (
     disp int NOT NULL DEFAULT 0
 );
 
+-- ---Insertion for example and tests
+DELETE FROM projetWeb.users;
+ALTER TABLE projetWeb.users AUTO_INCREMENT = 1;
+
+DELETE FROM projetWeb.edits;
+ALTER TABLE projetWeb.edits AUTO_INCREMENT = 1;
+
 -- projet.users
 
-INSERT INTO projet.users(username,pwd) VALUES("admin","admin"); -- administrateur
-INSERT INTO projet.users(username,pwd) VALUES("prof1","prof1"); -- prof1
-INSERT INTO projet.users(username,pwd) VALUES("prof2","prof2"); -- prof2
-INSERT INTO projet.users(username,pwd) VALUES("prof3","prof3"); -- prof3
-INSERT INTO projet.users(username,pwd) VALUES("prof4","prof4"); -- prof4
-INSERT INTO projet.users(username,pwd) VALUES("prof5","prof5"); -- prof5
+INSERT INTO projetWeb.users(username,pwd) VALUES("admin","admin"); -- administrateur
+INSERT INTO projetWeb.users(username,pwd) VALUES("prof1","prof1"); -- prof1
+INSERT INTO projetWeb.users(username,pwd) VALUES("prof2","prof2"); -- prof2
+INSERT INTO projetWeb.users(username,pwd) VALUES("prof3","prof3"); -- prof3
+INSERT INTO projetWeb.users(username,pwd) VALUES("prof4","prof4"); -- prof4
+INSERT INTO projetWeb.users(username,pwd) VALUES("prof5","prof5"); -- prof5
 
 -- projet.edits
 
-INSERT INTO projet.edits(name,desc) VALUES ("informatik","salle informatique");
-INSERT INTO projet.edits(name,desc) VALUES ("reunion","salle de réunion");
-INSERT INTO projet.edits(name,desc) VALUES ("calculator","temps de calcul sur un super calculateur");
-INSERT INTO projet.edits(name,desc) VALUES ("amphi","salle de conférence");
-INSERT INTO projet.edits(name,desc) VALUES ("kino","salle de vidéo projection");
-INSERT INTO projet.edits(name,desc) VALUES ("hdmi3","cable hdmi de 3 mètres");
+INSERT INTO projetWeb.edits(name,desc) VALUES ("informatik","salle informatique");
+INSERT INTO projetWeb.edits(name,desc) VALUES ("reunion","salle de réunion");
+INSERT INTO projetWeb.edits(name,desc) VALUES ("calculator","temps de calcul sur un super calculateur");
+INSERT INTO projetWeb.edits(name,desc) VALUES ("amphi","salle de conférence");
+INSERT INTO projetWeb.edits(name,desc) VALUES ("kino","salle de vidéo projection");
+INSERT INTO projetWeb.edits(name,desc) VALUES ("hdmi3","cable hdmi de 3 mètres");
